eb306710259c3724b98d95581961208befa6ff07
NalessoAxel <axelnalesso@gmail.com>
2021-04-12 14:16:03 +0200

    init

---
+++ build/precache-manifest.11c7a44494bec6ed9800eec134164fed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b86610dd0d89b0419fbae36ad27730d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "09ec05dd76226930803f",
+    "url": "/portfolio/static/css/main.2f43a0d8.chunk.css"
+  },
+  {
+    "revision": "755d460b9fabbd3b9913",
+    "url": "/portfolio/static/js/2.4ab6ee71.chunk.js"
+  },
+  {
+    "revision": "ce31aa0c83ef4cfc20b854276834e8d6",
+    "url": "/portfolio/static/js/2.4ab6ee71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ec05dd76226930803f",
+    "url": "/portfolio/static/js/main.f367983e.chunk.js"
+  },
+  {
+    "revision": "7ad40a0c761abff5b8b6",
+    "url": "/portfolio/static/js/runtime-main.7467a613.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/portfolio/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/portfolio/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "1bf915dcb4169fba8022bb8b5cdccc71",
+    "url": "/portfolio/static/media/breaking-bad.1bf915dc.png"
+  },
+  {
+    "revision": "0b8228419573d120d3295dd8da6f0c4f",
+    "url": "/portfolio/static/media/calculator.0b822841.png"
+  },
+  {
+    "revision": "bf7c3408312f65f52a23f17c161404f3",
+    "url": "/portfolio/static/media/cv_english.bf7c3408.pdf"
+  },
+  {
+    "revision": "0bb7792c144ba63829c2033f39eb7c57",
+    "url": "/portfolio/static/media/elodie-portfolio.0bb7792c.png"
+  },
+  {
+    "revision": "4105b7b33f369b86bf0994f29ea4f63c",
+    "url": "/portfolio/static/media/library.4105b7b3.png"
+  },
+  {
+    "revision": "c702ca985e4d6023f35c534f106c1805",
+    "url": "/portfolio/static/media/maison-records.c702ca98.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/portfolio/static/media/up-arrow-circle.d6c4e78d.svg"
+  }
+]);
